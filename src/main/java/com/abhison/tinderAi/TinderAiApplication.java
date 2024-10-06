3937e3d584bb1e152fe90463aa5e650abe4eff82
sonka <sonkar.abhishek45@gmail.com>
2024-10-07 01:18:06 +0530

    tinderAi : app in working state - initial commits

---
+++ src/main/java/com/abhison/tinderAi/TinderAiApplication.java
@@ -0,0 +1,13 @@
+package com.abhison.tinderAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhison/tinderAi/TinderAiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.abhison.tinderAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test");
+	}
+
+}
